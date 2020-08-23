4705c7e3d3a6eb6edad27ef2edfa65cef1e9d28c
Daria Lopteva <daria.lopteva@abbyy.com>
2020-08-23 20:35:05 +0300

    Задача на программирование: последняя цифра большого числа Фибоначчи

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,9 @@
 def fib(n):
+    '''
+    Return n-th Fibonacci number
+    :param n: index of a Fibonacci number
+    :return: number with index n from the Fibonacci sequence
+    '''
     fib_l = list()
     fib_l.append(0)
     fib_l.append(1)
@@ -7,9 +12,24 @@ def fib(n):
     return fib_l[n]
 
 
+def fib_digit(n):
+    '''
+    Return last digit of n-th Fibonacci number
+    :param n: index of Fibonacci number
+    :return: Last digit of a Fibonacci number with index n
+    '''
+    fib_d = list()
+    fib_d.append(0)
+    fib_d.append(1)
+    for i in range(2, n + 1):
+        fib_d.append((fib_d[i-2] + fib_d[i-1]) % 10)
+    return fib_d[n]
+
+
 def main():
     n = int(input())
-    print(fib(n))
+    # print(fib(n))
+    print(fib_digit(n))
 
 
 if __name__ == "__main__":
